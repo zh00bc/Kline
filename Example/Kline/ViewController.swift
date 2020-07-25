2ccf5d79fc958f2950525571b88bb42c0123d62b
zhangj <seasonsv@gmail.com>
2020-07-25 20:57:04 +0800

    init

---
+++ Example/Kline/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Kline
+//
+//  Created by zhangj on 07/25/2020.
+//  Copyright (c) 2020 zhangj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
